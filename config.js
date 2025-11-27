f0b2d5c142f9504c84c0f64b8a0ad25c48ef1152
Fraven Sancho Lazo <50414454+Fravens7@users.noreply.github.com>
2025-11-27 14:41:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: 'process.env.SUPABASE_URL',
-  key: 'process.env.SUPABASE_KEY'
+  url: 'https://vbcdlzmfysefgubthppt.supabase.co',
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiY2Rsem1meXNlZmd1YnRocHB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyMjg0NDgsImV4cCI6MjA3OTgwNDQ0OH0.ZuzVgebJW-hQYQr2a7kacU5YvavzZ4B0VmqCAcXbl4o'
 }
